49982f678288d4041e14ba3f46db2011e46f19fe
Diego P. <diego.velez@ircnl.gob.mx>
2024-07-26 16:13:21 -0600

    Update Prueba Foto.py

--- pages/Prueba Foto.py
+++ pages/Prueba Foto.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-if st.query_params["foliohash"] != None:
+if(st.query_params.__contains__("foliohash")):
     st.write("Hola.")
 else:
     st.write("Pon un folio no seas wey")
