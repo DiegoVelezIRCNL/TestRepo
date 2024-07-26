87a561fdab5f0db6fc0b7230e7331e65a2e1e745
Diego P. <diego.velez@ircnl.gob.mx>
2024-07-26 16:38:50 -0600

    Cambiesillo

--- models/models.py
+++ models/models.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+from typing import Optional
+
 class DatoRegistro:
     def __init__(self, Volumen:str, Libro:str, Inscripcion:str, Seccion:str, Municipio:str, Anio:str):
         self.Volumen = Volumen
@@ -6,3 +9,78 @@ class DatoRegistro:
         self.Seccion = Seccion
         self.Municipio = Municipio
         self.Anio = Anio
+
+class ModeloAlertaCompleta:
+    def __init__(self):
+        self.id_consecutivo: int = 0
+        self.FolioSolicitudPrecaptura: Optional[int] = None
+        self.FolioAlertaInmobiliaria: Optional[int] = None
+        self.FolioAlertaCatastral: Optional[int] = None
+        self.FechaRegistro: Optional[datetime] = None
+        self.nota_precaptura: Optional[str] = None
+        self.idLibro: Optional[str] = None
+        self.FolioAlertaForanea: Optional[int] = None
+        self.FolioHash: str = ""
+
+class ModeloPrecaptura:
+    def __init__(self):
+        self.id: str = ""
+        self.AlertaInmobiliaria: bool = False
+        self.AlertaCatastral: bool = False
+        self.registro_inmobiliaria_volumen: Optional[str] = None
+        self.registro_inmobiliaria_libro: Optional[str] = None
+        self.registro_inmobiliaria_inscripcion: Optional[str] = None
+        self.registro_inmobiliaria_municipio: Optional[str] = None
+        self.registro_inmobiliaria_seccion: Optional[str] = None
+        self.registro_inmobiliaria_anio: str = ""
+        self.datos_solicitante_nombres: Optional[str] = None
+        self.datos_solicitante_apellidoPaterno: Optional[str] = None
+        self.datos_solicitante_apellidoMaterno: Optional[str] = None
+        self.datos_solicitante_CURP: Optional[str] = None
+        self.datos_solicitante_razonSocial: Optional[str] = None
+        self.datos_solicitante_RFC: str = ""
+        self.datos_solicitante_correoPrimario: str = ""
+        self.datos_solicitante_correoSecundario: Optional[str] = ""
+        self.datos_solicitante_numeroCelular: str = ""
+        self.registro_catastral_expedienteCatastral: Optional[str] = None
+        self.registro_titular_nombre: Optional[str] = None
+        self.registro_titular_curp: Optional[str] = None
+        self.EstatusSolicitud: str = "EN REVISIÓN"
+        self.UsuarioAprobacion: str = "N/A"
+        self.datos_notario_correo: Optional[str] = None
+        self.pago_referencia: Optional[str] = None
+        self.datos_notario_numeroNotaria: int = -1
+        self.datos_precaptura_fechaSolicitud: datetime = datetime.now()
+        self.estatus_observacion: Optional[str] = ""
+        self.registro_catastral_observacion: str = ""
+        self.registro_inmobiliaria_observacion: str = ""
+        self.referencia_asociada: Optional[str] = ""
+        self.id_notario: Optional[int] = None
+        self.registro_folioreal: Optional[str] = None
+        self.correo_misi_registrante: Optional[str] = None
+        self.id_provisional: Optional[str] = None
+        self.datosComprador: Optional['DatosComprador_HerenciaAlerta'] = None
+        self.tipoPrecaptura: Optional[str] = ""
+        self.FolioFisico: Optional[str] = ""
+
+class DatosComprador_HerenciaAlerta:
+    def __init__(self):
+        self.id: Optional[int] = None
+        self.FolioPrecaptura: Optional[str] = None
+        self.datos_solicitante_nombres: str = ""
+        self.datos_solicitante_apellidoPaterno: str = ""
+        self.datos_solicitante_apellidoMaterno: str = ""
+        self.datos_solicitante_CURP: str = ""
+        self.datos_solicitante_RFC: str = ""
+        self.datos_solicitante_correoPrimario: str = ""
+        self.datos_solicitante_correoSecundario: str = ""
+        self.datos_solicitante_numeroCelular: str = ""
+        self.EstatusSolicitud: str = ""
+        self.NuevoDato: str = ""
+        self.datos_solicitante_razonSocial: Optional[str] = None
+        self.datos_copropietarios: bool = False
+
+class ObjDatosAlerta:
+    def __init__(self):
+        self.modeloPrecaptura: ModeloPrecaptura = None
+        self.modeloAlertaCompleta: ModeloAlertaCompleta = None
--- pages/Prueba Foto.py
+++ pages/Prueba Foto.py
@@ -1,6 +1,34 @@
+import json
+import requests
 import streamlit as st
 
+from models.models import ObjDatosAlerta
+
+# Obtener archivo
+def obtenerDatosAlerta(hashFolio):
+    url = "https://localhost:44337/api/InformativoAlertas/ObtenerDatos_Alerta"
+    response = requests.get(
+        url, 
+        params={
+            'hashFolio': hashFolio,
+        }
+        )
+    dump = json.dumps(response.content)
+    
+    ObjDatosAlerta = json.load(response.content)
+    return ObjDatosAlerta
+
+objDatosAlerta = ObjDatosAlerta.__init__()
+
+st.title("Validador de Alerta")
+
+
 if(st.query_params.__contains__("foliohash")):
-    st.write("Hola.")
+    obtenerDatosAlerta(st.query_params["foliohash"])
+    st.write("Alerta")
+
+    if(objDatosAlerta != ObjDatosAlerta.__init__()):
+        st.write(objDatosAlerta.modeloPrecaptura.id)
+
 else:
     st.write("Pon un folio no seas wey")
